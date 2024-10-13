3f6d8e38d66285e7c6c0f631bd102079d361853c
AlanFernandes86 <josefernandes.alan@gmail.com>
2024-10-13 18:17:50 -0300

    criando e inicializando database junto com o docker compose

---
+++ init-db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Aguardando o SQL Server iniciar..."
+sleep 10
+until /opt/mssql-tools/bin/sqlcmd -S sqlserver,1433 -U SA -P "SqlServer2019!" -Q "SELECT 1" &> /dev/null; do
+    echo "SQL Server não está pronto. Aguardando..."
+    sleep 5
+done
+
+echo "SQL Server está pronto. Executando o script SQL..."
+
+/opt/mssql-tools/bin/sqlcmd -S sqlserver,1433 -U SA -P "SqlServer2019!" -d master -i /usr/src/app/CreateAndInitDatabase.sql
+
+echo "Script SQL executado com sucesso."
